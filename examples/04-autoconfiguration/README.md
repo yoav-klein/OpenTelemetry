7ee0ede868b20fe2c27b4fc12b9b3b80b0a5bce0
yoklein <yoav.klein@cyberark.com>
2024-12-30 13:43:40 +0200

    Auto-configuration

---
+++ examples/04-autoconfiguration/README.md
@@ -0,0 +1,5 @@
+# Autoconfiguration of SDK
+---
+
+Here we demonstrate using zero-code configuration of the SDK rather than programmatically configuring it.
+
---
+++ examples/04-autoconfiguration/app/src/main/java/org/example/OpenTelemetryConfig.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
+import io.opentelemetry.exporter.jaeger.JaegerGrpcSpanExporter;
+import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor;
+import io.opentelemetry.exporter.logging.LoggingSpanExporter;
+import io.opentelemetry.sdk.resources.Resource;
+
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
+
+public class OpenTelemetryConfig {
+  public static OpenTelemetry autoconfiguredSdk() {
+    return AutoConfiguredOpenTelemetrySdk.initialize().getOpenTelemetrySdk();
+  }
+}
+
+
