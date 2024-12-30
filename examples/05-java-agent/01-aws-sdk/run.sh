c6382ba68aa17433d5e1647018fa250460f12994
Yoav Klein <yoavklein25@gmail.com>
2024-12-30 20:23:53 +0000

    Using OTLP in java agent

--- examples/05-java-agent/01-aws-sdk/run.sh
+++ examples/05-java-agent/01-aws-sdk/run.sh
@@ -5,7 +5,11 @@ export AWS_SECRET_ACCESS_KEY=xxx
 export S3_KEY=some-object
 export S3_BUCKET=my=bucket
 
-export OTEL_TRACES_EXPORTER=console
+
+export OTEL_TRACES_EXPORTER=otlp
 export OTEL_METRICS_EXPORTER=none
+export OTEL_SERVICE_NAME=my-service
+export OTEL_EXPORTER_OTLP_ENDPOINT=http://localhost:4318
+
 
 ./gradlew run
--- examples/05-java-agent/02-using-annotations/run.sh
+++ examples/05-java-agent/02-using-annotations/run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 
-export OTEL_TRACES_EXPORTER=console
+export OTEL_TRACES_EXPORTER=otlp
 export OTEL_METRICS_EXPORTER=none
+export OTEL_SERVICE_NAME=my-service
+export OTEL_EXPORTER_OTLP_ENDPOINT=http://localhost:4318
 
 ./gradlew run
