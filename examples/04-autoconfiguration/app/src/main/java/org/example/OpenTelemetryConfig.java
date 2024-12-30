a4bebafc5677f915dd240c8c891c0236991597c8
Yoav Klein <yoavklein25@gmail.com>
2024-12-30 14:44:58 +0000

    AutoConfiguration

--- examples/04-autoconfiguration/app/src/main/java/org/example/OpenTelemetryConfig.java
+++ examples/04-autoconfiguration/app/src/main/java/org/example/OpenTelemetryConfig.java
@@ -9,17 +9,21 @@ import io.opentelemetry.exporter.jaeger.JaegerGrpcSpanExporter;
 import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor;
 import io.opentelemetry.exporter.logging.LoggingSpanExporter;
 import io.opentelemetry.sdk.resources.Resource;
+import io.opentelemetry.sdk.trace.export.SpanExporter;
 
 import io.opentelemetry.sdk.OpenTelemetrySdk;
 import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
 
 public class OpenTelemetryConfig {
   public static OpenTelemetry initialize() {
-    return AutoConfiguredOpenTelemetrySdk.builder().
-    addSpanExporterCustomizer((spanExporter, configProperties) -> {
-      return JaegerGrpcSpanExporter.builder()
+    return AutoConfiguredOpenTelemetrySdk.builder()
+    .addSpanExporterCustomizer((spanExporter, configProperties) -> {
+      SpanExporter jaeger =  JaegerGrpcSpanExporter.builder()
       .setEndpoint("http://localhost:14250") // Replace with your Jaeger endpoint
       .build();
+
+      //return SpanExporter.composite(jaeger, spanExporter);
+      return jaeger;
     })
     .build().getOpenTelemetrySdk();
   }
--- examples/04-autoconfiguration/run.sh
+++ examples/04-autoconfiguration/run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-export OTEL_TRACES_EXPORTER=jaeger-exporter
+export OTEL_TRACES_EXPORTER=none
 export OTEL_METRICS_EXPORTER=none
-
+export OTEL_LOGS_EXPORTER=none
+export OTEL_SERVICE_NAME=my-service
 
 ./gradlew run
