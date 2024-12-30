7a3ee44e9d14928b44b85da1fc9ee32e31948c41
yoklein <yoav.klein@cyberark.com>
2024-12-30 19:45:23 +0200

    Java agent docs

--- examples/05-java-agent/run.sh
+++ examples/05-java-agent/run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+export AWS_ACCESS_KEY_ID=xxx
+export AWS_SECRET_ACCESS_KEY=xxx
+export S3_KEY=some-object
+export S3_BUCKET=my=bucket
+
 export OTEL_TRACES_EXPORTER=console
 export OTEL_METRICS_EXPORTER=none
 
