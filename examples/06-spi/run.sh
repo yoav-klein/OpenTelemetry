ed32e750041797298832b1cc34ae331bbfa8c6c6
yoklein <yoav.klein@cyberark.com>
2024-12-31 07:06:39 +0000

    Delete unnecessary

--- examples/06-spi/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-export OTEL_TRACES_EXPORTER=jaeger-exporter
-export OTEL_METRICS_EXPORTER=none
-
-./gradlew run
--- examples/test/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export OTEL_TRACES_EXPORTER=console
-export OTEL_METRICS_EXPORTER=none
-export OTEL_LOGS_EXPORTER=none
-export OTEL_SERVICE_NAME=my-service
-
-./gradlew run
