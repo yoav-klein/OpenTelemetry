7cc8d53c9da5918becf5d8fbd90cc06d93325026
yoklein <yoav.klein@cyberark.com>
2025-01-02 17:24:53 +0200

    Logging

---
+++ examples/08-logging/app/src/main/java/org/example/OpenTelemetryConfig.java
@@ -0,0 +1,55 @@
+package org.example;
+
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.exporter.otlp.http.trace.OtlpHttpSpanExporter;
+import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor;
+import io.opentelemetry.sdk.resources.Resource;
+import io.opentelemetry.sdk.logs.SdkLoggerProvider;
+import io.opentelemetry.sdk.logs.export.SimpleLogRecordProcessor;
+import io.opentelemetry.exporter.otlp.http.logs.OtlpHttpLogRecordExporter;
+
+public class OpenTelemetryConfig {
+
+    public static OpenTelemetry initialize() {
+
+        String otelExporterEndpoint = System.getenv("OTEL_EXPORTER_OTLP_ENDPOINT");
+        
+        if(otelExporterEndpoint == null) {
+            otelExporterEndpoint = "http://localhost:4318";
+        }
+
+        String otelTracesEndpoint = otelExporterEndpoint + "/v1/traces";
+        String otelLoggingEndpoint = otelExporterEndpoint + "/v1/logging";
+       
+        SimpleSpanProcessor spanProcessor = SimpleSpanProcessor.builder(OtlpHttpSpanExporter.builder()
+                .setEndpoint(otelTracesEndpoint)
+                .build())
+            .build();
+        
+        // Set up Tracer Provider
+        Resource myApp = Resource.getDefault().toBuilder().put("service.name", "basic-example").build();
+
+        SdkTracerProvider tracerProvider = SdkTracerProvider.builder()
+            .setResource(myApp)
+            .addSpanProcessor(spanProcessor)
+            .build();
+
+        OtlpHttpLogRecordExporter exporter = OtlpHttpLogRecordExporter.builder().setEndpoint(endpoint).build();
+        
+        SdkLoggerProvider loggerProvider = SdkLoggerProvider.builder()
+            .setResource(myApp)
+            .addLogRecordProcessor(SimpleLogRecordProcessor.create(exporter))
+            .build();
+
+        // Set up OpenTelemetry SDK
+        OpenTelemetrySdk openTelemetrySdk = OpenTelemetrySdk.builder()
+            .setTracerProvider(tracerProvider)
+            .setLoggerProvider(loggerProvider)
+            .build();
+
+        return openTelemetrySdk;
+    }
+}
+
