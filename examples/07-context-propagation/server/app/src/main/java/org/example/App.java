8335aaaaf21e0918b8b6522b6b499241ffc2df52
yoklein <yoav.klein@cyberark.com>
2025-01-02 13:18:45 +0200

    Server

--- examples/07-context-propagation/server/app/src/main/java/org/example/App.java
+++ examples/07-context-propagation/server/app/src/main/java/org/example/App.java
@@ -3,24 +3,83 @@
  */
 package org.example;
 
+import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpExchange;
+
+import io.opentelemetry.api.baggage.propagation.W3CBaggagePropagator;
+import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
+import io.opentelemetry.context.Context;
+import io.opentelemetry.context.Scope;
+import io.opentelemetry.context.propagation.ContextPropagators;
+import io.opentelemetry.context.propagation.TextMapGetter;
+import io.opentelemetry.context.propagation.TextMapPropagator;
+
 import java.net.InetSocketAddress;
+import java.util.List;
+import java.io.IOException;
 import java.io.OutputStream;
 
 public class App {
 
+    private static TextMapGetter<HttpExchange> TEXT_MAP_GETTER = new HttpRequestGetter();
+
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
 
-        server.createContext("/", httpExchange -> {
-            String response = "Hello from server";
-            httpExchange.sendResponseHeaders(200, response.length());
-            OutputStream os = httpExchange.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
-        });
+        ContextPropagators propagators = ContextPropagators.create(
+            TextMapPropagator.composite(
+                W3CTraceContextPropagator.getInstance(), W3CBaggagePropagator.getInstance()));
+
+        server.createContext("/", new Handler(propagators));
 
         server.start();
 
+    }
+
+    private static class Handler implements HttpHandler {
+        private ContextPropagators propagators;
+
+        public Handler(ContextPropagators propagators) {
+            this.propagators = propagators;
+        }
+
+        @Override
+        public void handle(HttpExchange exchage) throws IOException {
+            Context extractedContext = propagators.getTextMapPropagator().extract(Context.current(), exchage, TEXT_MAP_GETTER);
+
+            try(Scope scope = extractedContext.makeCurrent()) {
+                
+            } finally {
+                String response = "Hello from server";
+                exchage.sendResponseHeaders(200, response.length());
+                OutputStream os = exchage.getResponseBody();
+                os.write(response.getBytes());
+                os.close();
+            }
+
+        }
+
+    }
+
+    private static class HttpRequestGetter implements TextMapGetter<HttpExchange> {
+        @Override
+        public Iterable<String> keys(HttpExchange exchange) {
+            return exchange.getRequestHeaders().keySet();
+        }
+
+        @Override
+        public String get(HttpExchange exchange, String key) {
+            if(exchange == null) {
+                return null;
+            }
+            List<String> headers = exchange.getRequestHeaders().get(key);
+            if(headers == null || headers.isEmpty()) {
+                return null;
+            }
+            
+            return headers.get(0);
+        }
+        
     }
 }
